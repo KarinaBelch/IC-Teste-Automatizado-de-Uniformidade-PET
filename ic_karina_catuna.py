872665dfd337852dfeea3aa48273ddcbf89a8ad6
KarinaBelch <73723978+KarinaBelch@users.noreply.github.com>
2025-07-31 15:53:08 -0300

    Update ic_karina_catuna.py

--- ic_karina_catuna.py
+++ ic_karina_catuna.py
@@ -518,6 +518,9 @@ if uploaded_zip:
      edges_volume, filled_volume, raio_volume, cx_volume, cy_volume = funcPreencherVolume(volume)
      edges_volume, filled_volume, raio_volume, cx_volume, cy_volume = funcPopularArrays(edges_volume, filled_volume, raio_volume, cx_volume, cy_volume)
      imagem_mascara = funcCriarMascara(volume, filled_volume)
+     st.markdown(edges_volume)
+     st.markdown(filled_volume)
+     st.markdown(raio_volume)
 
      image = volume[idx]
 
